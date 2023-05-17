fc705f3ca4eccb16fc5695a8a502172e0947a8a7
frudpredict <133844588+frudpredict@users.noreply.github.com>
2023-05-17 16:06:53 +0530

    Update Homepage.py

--- Homepage.py
+++ Homepage.py
@@ -2,8 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-with open('style.css') as f:
-    st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
+
 
 
 st.set_page_config(
