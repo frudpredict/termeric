0fd8878f688f990f2c2dd410d9d400e6216d5b41
frudpredict <133844588+frudpredict@users.noreply.github.com>
2023-05-17 12:28:54 +0530

    Add files via upload

---
+++ pages/Dryness Level Prediction.py
@@ -0,0 +1,40 @@
+import streamlit as st
+import cv2
+import numpy as np
+import tensorflow as tf
+from tensorflow.keras.preprocessing import image
+from tensorflow.keras.applications.mobilenet_v2 import MobileNetV2,preprocess_input as mobilenet_v2_preprocess_input
+
+model = tf.keras.models.load_model("saved_model/dryness.hdf5")
+### load file
+uploaded_file = st.file_uploader("Choose a image file")
+
+map_dict = {0: 'Half_Dry',
+            1: 'More_dry',
+            2: 'Dried'
+            }
+
+
+if uploaded_file is not None:
+    # Convert the file to an opencv image.
+    file_bytes = np.asarray(bytearray(uploaded_file.read()), dtype=np.uint8)
+    opencv_image = cv2.imdecode(file_bytes, 1)
+    opencv_image = cv2.cvtColor(opencv_image, cv2.COLOR_BGR2RGB)
+    resized = cv2.resize(opencv_image,(224,224))
+    # Now do something with the image! For example, let's display it:
+    st.image(opencv_image, channels="RGB")
+
+    resized = mobilenet_v2_preprocess_input(resized)
+    img_reshape = resized[np.newaxis,...]
+
+    Genrate_pred = st.button("Dryness Level")    
+    if Genrate_pred:
+        prediction = model.predict(img_reshape).argmax()
+        st.title("Predicted Dryness Level for the image is {}".format(map_dict [prediction]))
+        if(map_dict [prediction] == 'Half_Dry'):
+            st.text('You need to dry more days to make turmeric powders')
+        if(map_dict [prediction] == 'More_dry'):
+            st.text('You can not make turmeric powders. Because Your turmerics are More dried')
+        if(map_dict [prediction] == 'Dried'):
+            st.text('Now you can make turmeric powders')
+           
