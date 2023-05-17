48c476ad6687ca9adff96be8118b33d1bbf93c2c
frudpredict <133844588+frudpredict@users.noreply.github.com>
2023-05-17 16:05:07 +0530

    Update Homepage.py

--- Homepage.py
+++ Homepage.py
@@ -2,6 +2,10 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
+with open('style.css') as f:
+    st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
+
+
 st.set_page_config(
     page_title="Multipage App",
     page_icon="👋",
