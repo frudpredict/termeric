a4818787ee0aece1deec2b4a9783a3e5b06a7684
frudpredict <133844588+frudpredict@users.noreply.github.com>
2023-05-17 12:53:08 +0530

    Update Disease Identification.py

--- pages/Disease Identification.py
+++ pages/Disease Identification.py
@@ -10,7 +10,7 @@ model = tf.keras.models.load_model("saved_model/diseases.hdf5")
 col1, col2 = st.columns([1, 1])
 
 col1.subheader("Disease Identification")
-uploaded_file = col1.st.file_uploader("Choose a image file")
+uploaded_file = st.file_uploader("Choose a image file")
 
 col2.subheader("A narrow column with the data")
 
