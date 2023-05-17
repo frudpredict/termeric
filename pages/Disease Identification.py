6fd1ba92eec98085fee93fb5454aa5d1f5a6ef3c
frudpredict <133844588+frudpredict@users.noreply.github.com>
2023-05-17 14:46:28 +0530

    Update Disease Identification.py

--- pages/Disease Identification.py
+++ pages/Disease Identification.py
@@ -34,17 +34,16 @@ if uploaded_file is not None:
 
     Genrate_pred = st.button("Disease")    
     if Genrate_pred:
-            col1, col2 = st.columns([1, 1])
             prediction = model.predict(img_reshape).argmax()
             st.title("Predicted Disease for the image is {}".format(map_dict [prediction]))
             if(map_dict [prediction] == 'Leaf Blotch'):
-                        col1.title('Leaf Blotch')
-                        col1.text('The fungus is mainly air borne and primary infection occurs on lower leaves with the inoculum surviving in dried leaves of host, left over in the field. The ascospores discharged from successively maturing asci infect fresh leaves without dormancy, thus causing secondary infection. Secondary infection is most dangerous than primary one causing profuse sprouting all over the leaves. The pathogen persists in summer by means of acrogenous cells on leaf debris, and desiccated ascospores and blastopores in soil and among fallen leaves.')
-                        col1.title('Recommndation')
-                        col1.text('Spray mancozeb @ 2.5 g/liter of water or Carbendazim @ 1g/liter; 2-3 sprays at fortnightly intervals.')
+                 st.title('Leaf Blotch')
+                 st.text('The fungus is mainly air borne and primary infection occurs on lower leaves with the inoculum surviving in dried leaves of host, left over in the field. The ascospores discharged from successively maturing asci infect fresh leaves without dormancy, thus causing secondary infection. Secondary infection is most dangerous than primary one causing profuse sprouting all over the leaves. The pathogen persists in summer by means of acrogenous cells on leaf debris, and desiccated ascospores and blastopores in soil and among fallen leaves.')
+                 st.title('Recommndation')
+                 st.text('Spray mancozeb @ 2.5 g/liter of water or Carbendazim @ 1g/liter; 2-3 sprays at fortnightly intervals.')
             if(map_dict [prediction] == 'Leaf Spot'):
-                        col1.title('Leaf Spot')
-                        col1.text('The fungus is carried on the scales of rhizomes which are the source of primary infection during sowing. The secondary spread is by wind, water and other physical and biological agents. The same pathogen is also reported to cause leaf-spot and fruit rot of chili where it is transmitted through seed borne infections. If chili is grown in nearby fields or used in crop rotation with turmeric, the pathogen perpetuates easily, building up inoculum potential for epiphytotic outbreaks.')
-                        col1.title('Recommndation')
-                        col1.markdown('Spray mancozeb @ 2.5 g/liter of water or carbendazim @ 1g/litre; 2-3 sprays at fortnightly intervals.The infected and dried leaves should be collected and burnt in order to reduce the inoculum source in the field.')
+                 st.title('Leaf Spot')
+                 st.text('The fungus is carried on the scales of rhizomes which are the source of primary infection during sowing. The secondary spread is by wind, water and other physical and biological agents. The same pathogen is also reported to cause leaf-spot and fruit rot of chili where it is transmitted through seed borne infections. If chili is grown in nearby fields or used in crop rotation with turmeric, the pathogen perpetuates easily, building up inoculum potential for epiphytotic outbreaks.')
+                 st.title('Recommndation')
+                 st.markdown('Spray mancozeb @ 2.5 g/liter of water or carbendazim @ 1g/litre; 2-3 sprays at fortnightly intervals.The infected and dried leaves should be collected and burnt in order to reduce the inoculum source in the field.')
             
