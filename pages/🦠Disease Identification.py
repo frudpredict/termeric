73c5a8329a915cef5c38d56ad4767d7a22b013cd
frudpredict <133844588+frudpredict@users.noreply.github.com>
2023-05-18 18:03:44 +0530

    Update 🦠Disease Identification.py

--- pages/🦠Disease Identification.py
+++ pages/🦠Disease Identification.py
@@ -4,45 +4,20 @@ import numpy as np
 import tensorflow as tf
 from tensorflow.keras.preprocessing import image
 from tensorflow.keras.applications.mobilenet_v2 import MobileNetV2,preprocess_input as mobilenet_v2_preprocess_input
-from streamlit_cropper import st_cropper
-from PIL import Image
-st.set_option('deprecation.showfileUploaderEncoding', False)
-
-img_file = st.file_uploader("Choose a image file")
-realtime_update = st.sidebar.checkbox(label="Update in Real Time", value=True)
-box_color = st.sidebar.color_picker(label="Box Color", value='#0000FF')
-aspect_choice = st.sidebar.radio(label="Aspect Ratio", options=["1:1", "16:9", "4:3", "2:3", "Free"])
-aspect_dict = {
-    "1:1": (1, 1),
-    "16:9": (16, 9),
-    "4:3": (4, 3),
-    "2:3": (2, 3),
-    "Free": None
-}
-aspect_ratio = aspect_dict[aspect_choice]
 
 model = tf.keras.models.load_model("saved_model/diseases.hdf5")
+### load file
+uploaded_file = st.file_uploader("Choose a image file")
+
 map_dict = {
             0:'Leaf Blotch',
             1:'Leaf Spot'
             }
 
-if img_file:
-    img = Image.open(img_file)
-    if not realtime_update:
-        st.write("Double click to save crop")
-    # Get a cropped image from the frontend
-    cropped_img = st_cropper(img, realtime_update=realtime_update, box_color=box_color,
-                                aspect_ratio=aspect_ratio)
-    
-    # Manipulate cropped image at will
-    st.write("Preview")
-    _ = cropped_img.thumbnail((150,150))
-    st.image(cropped_img)
+
+if uploaded_file is not None:
     # Convert the file to an opencv image.
-if img_file is not None:
-    # Convert the file to an opencv image.
-    file_bytes = np.asarray(bytearray(img_file.read()), dtype=np.uint8)
+    file_bytes = np.asarray(bytearray(uploaded_file.read()), dtype=np.uint8)
     opencv_image = cv2.imdecode(file_bytes, 1)
     opencv_image = cv2.cvtColor(opencv_image, cv2.COLOR_BGR2RGB)
     resized = cv2.resize(opencv_image,(224,224))
