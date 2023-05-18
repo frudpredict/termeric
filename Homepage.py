370570e0e1336b7073358b9ce64d06a42516592b
frudpredict <133844588+frudpredict@users.noreply.github.com>
2023-05-19 05:15:54 +0530

    Update Homepage.py

--- Homepage.py
+++ Homepage.py
@@ -16,5 +16,4 @@ st.sidebar.success("Select a page above.")
 image = Image.open('top.jpg')
 
 st.image(image, caption='@Yellow Root')
-
-
+st.markdown('Welcome to YellowRoot, a smart and intelligent system designed to maximize turmeric cultivation in Sri Lanka. Our web app offers four essential components powered by machine learning to revolutionize the turmeric industry. Whether youre a farmer, researcher, or enthusiast, YellowRoot is here to provide you with valuable insights and recommendations for enhancing turmeric growth. ')
