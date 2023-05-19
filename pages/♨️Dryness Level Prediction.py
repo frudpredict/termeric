73f3fcd3242ec58f414ddc0b57456788000c3dbf
frudpredict <133844588+frudpredict@users.noreply.github.com>
2023-05-19 10:47:47 +0530

    Update ♨️Dryness Level Prediction.py

--- pages/♨️Dryness Level Prediction.py
+++ pages/♨️Dryness Level Prediction.py
@@ -9,10 +9,10 @@ model = tf.keras.models.load_model("saved_model/dryness.hdf5")
 ### load file
 uploaded_file = st.file_uploader("Choose a image file")
 
-map_dict = {0:'Half Dry2',
+map_dict = {0:'Half Dry1',
             1:'Toodryturmeric',
-            2:'Half Dry',
-            3:'NOT',
+            2:'Not',
+            3:'Half Dry2',
             4:'Driedturmeric'
             }
 
