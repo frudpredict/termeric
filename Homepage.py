2e29e3cba6897ed4bc76c311912f02a8e0168328
frudpredict <133844588+frudpredict@users.noreply.github.com>
2023-05-19 05:51:52 +0530

    Update Homepage.py

--- Homepage.py
+++ Homepage.py
@@ -17,3 +17,4 @@ image = Image.open('top.jpg')
 
 st.image(image, caption='@Yellow Root')
 st.markdown('Welcome to YellowRoot, a smart and intelligent system designed to maximize turmeric cultivation in Sri Lanka. Our web app offers four essential components powered by machine learning to revolutionize the turmeric industry. Whether youre a farmer, researcher, or enthusiast, YellowRoot is here to provide you with valuable insights and recommendations for enhancing turmeric growth. ')
+st.markdown('1.Disease Identification and Recommendations: With our advanced machine learning algorithms, YellowRoot can accurately identify and classify turmeric leaf diseases. By uploading images of diseased leaves, our system quickly analyzes the symptoms and provides tailored recommendations for disease management. This helps farmers take proactive measures to prevent crop loss and optimize turmeric health.')
