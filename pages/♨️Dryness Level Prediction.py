e7bff4b332354fbe5db5a6dcca1309fdd3ad306a
frudpredict <133844588+frudpredict@users.noreply.github.com>
2023-05-19 05:34:14 +0530

    Update ♨️Dryness Level Prediction.py

--- pages/♨️Dryness Level Prediction.py
+++ pages/♨️Dryness Level Prediction.py
@@ -34,13 +34,13 @@ if uploaded_file is not None:
         prediction = model.predict(img_reshape).argmax()
         st.title("Predicted Dryness Level for the image is {}".format(map_dict [prediction]))
         if(map_dict [prediction] == 'Toodryturmeric'):
-            st.text('The turmeric in the uploaded image is too dry and cannot be used for making powders. ')
+            st.markdown('The turmeric in the uploaded image is too dry and cannot be used for making powders. ')
         if(map_dict [prediction] == 'Half Dry'):
-            st.text('The turmeric in the uploaded image has not dried enough to make a powder. You have to dry the turmeric for another 2 or 3 days to make the powders.')
+            st.markdown('The turmeric in the uploaded image has not dried enough to make a powder. You have to dry the turmeric for another 2 or 3 days to make the powders.')
         if(map_dict [prediction] == 'Driedturmeric'):
-            st.text('The turmeric in the uploaded image has dried enough to make powders')
+            st.markdown('The turmeric in the uploaded image has dried enough to make powders')
         if(map_dict [prediction] == 'Half Dry2'):
-            st.text('The turmeric in the uploaded image has not dried enough to make a powder. You have to dry the turmeric for another 1 or 2 days to make the powders. ')
+            st.markdown('The turmeric in the uploaded image has not dried enough to make a powder. You have to dry the turmeric for another 1 or 2 days to make the powders. ')
         if(map_dict [prediction] == 'NOT'):
-            st.text('Not a Turmeric')
+            st.markdown('Not a Turmeric')
            
